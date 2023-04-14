1c9179f9f60c32b7868b76e5b03445db585940f2
Arthur Augustinho <arthuraugustinho35@gmail.com>
2023-04-14 16:19:24 -0300

    Atualização de urls

--- Event Hive/usuarios/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def cadastro(request):
-    if request.method == "GET":
-        return render(request, 'cadastro.html')
-    elif request.method == "POST":
-        return HttpResponse('Testando')
