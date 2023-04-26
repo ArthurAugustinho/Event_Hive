f67558ebcb0735ebb502a3d68997e4f541ebca4c
Arthur Augustinho Dos Anjos <98282637+ArthurAugustinho@users.noreply.github.com>
2023-04-26 15:33:09 -0300

    Update models.py

--- eventos/models.py
+++ eventos/models.py
@@ -11,7 +11,7 @@ class Evento(models.Model):
     data_termino = models.DateField()
     carga_horaria = models.IntegerField()
     logo = models.FileField(upload_to="logos")
-    participantes = models.ManyToManyField(User, related_name="evento_participante", null=True, blank=True)
+    participantes = models.ManyToManyField(User, related_name="evento_participante", blank=True)
     
     #paleta de cores
     cor_principal = models.CharField(max_length=7)
